ecb430f309c91dc20729900cb41f7164f8177d5e
Binarri <binarridhawiritanaya0@gmail.com>
2026-05-28 14:27:12 +0700

    Add files via upload
    Pertemuan10 Selesai

---
+++ PBO010_24060124140143/jdbc_mhs_service/src/jdbc/utilities/MysqlUtility.java
@@ -0,0 +1,41 @@
+/** Binar Ridha Wiritanaya - 24060124140143
+ * File : MysqlUtility.java
+ * Deskripsi : Kelas utility untuk koneksi database MySQL
+ */
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jdbc.utilities;
+import java.sql.*;
+/**
+ *
+ * @author IDEAPET 5
+ */
+
+public class MysqlUtility {
+    //atribut
+    private static Connection koneksi;
+    
+    //Untuk mendapatkan koneksi database
+    public static Connection getConnection(){
+        if(koneksi == null){
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                String url = "jdbc:mysql://localhost:3306/pbo";
+                String user = "root";
+                String password = "";
+                koneksi = DriverManager.getConnection(url, user, password);
+                if(koneksi != null){
+                    System.out.println("Koneksi berhasil");
+                }
+            }catch(ClassNotFoundException cne){
+                System.out.println("Gagal load driver : " + cne.getMessage());
+            }catch(SQLException sqle){
+                System.out.println("Gagal Koneksi : " + sqle.getMessage());
+            }
+        }
+        return koneksi;
+    }
+}
